39a249b1ffed1e0cbdddb629e00502821cf1247d
Bakin Eugene <bakin.eugene@ya.ru>
2025-05-22 12:44:43 +0500

    Introduces more structs

--- main.c
+++ main.c
@@ -10,16 +10,26 @@ bool ONE_DEFINITION[1] = {true};
 bool FOUR_DEFINITION[4] = {true, true, true, true};
 bool G_BAR_DEFINITION[6] = {true, true, true, true, false, false};
 
+struct Position {
+    char x;
+    char y;
+};
+
 struct Piece {
     char size_x;
     char size_y;
     bool *definition;
 };
 
-struct Piece g_bar = {2, 3, (bool *) G_BAR_DEFINITION};
-struct Piece square = {2, 2, (bool *) FOUR_DEFINITION};
-struct Piece bar = {1, 4, (bool *) FOUR_DEFINITION};
-struct Piece bar_2 = {4, 1, (bool *) FOUR_DEFINITION};
+struct CurrentPiece {
+    struct Piece piece;
+    struct Position position;
+};
+
+const struct Piece g_bar = {2, 3, (bool *) G_BAR_DEFINITION};
+const struct Piece square = {2, 2, (bool *) FOUR_DEFINITION};
+const struct Piece bar = {1, 4, (bool *) FOUR_DEFINITION};
+const struct Piece bar_2 = {4, 1, (bool *) FOUR_DEFINITION};
 
 bool can_place_piece(
     bool *screen,
@@ -103,9 +113,11 @@ void check_board(
 
 static int game_time = 0;
 static bool running = true;
-static char piece_position_x = 3;
+struct Piece pieces[4] = {square, bar, bar_2, g_bar};
 
 int main(int argc, char** argv) {
+    srand(time(NULL));
+    
     renderer_init();
 
     bool board[SCREEN_X][SCREEN_Y];
@@ -113,16 +125,12 @@ int main(int argc, char** argv) {
 
     for (int x = 0; x < SCREEN_X; ++x) {
         for (int y = 0; y < SCREEN_Y; ++y) {
-	    board[x][y] = false;
-	}
+	        board[x][y] = false;
+	    }
     }
 
-    srand(time(NULL));
 
-    struct Piece pieces[4] = {square, bar, bar_2, g_bar};
-    char piece_number = rand() % 4;
-    struct Piece current_piece = pieces[piece_number];
-    char current_piece_y = -1;
+    struct CurrentPiece piece = {pieces[rand() % 4], {3, -1}};
     while (running) {
         enum Event event = EVENT_EMPTY;
         do {
@@ -132,21 +140,21 @@ int main(int argc, char** argv) {
                     running = false;
                         break;
                     case EVENT_LEFT:
-                        if (can_place_piece((bool *) board, piece_position_x - 1, current_piece_y, current_piece)) {
-                            --piece_position_x;
-                            place_piece((bool *) board, (bool *) screen, piece_position_x, current_piece_y, current_piece);
+                        if (can_place_piece((bool *) board, piece.position.x - 1, piece.position.y, piece.piece)) {
+                            --piece.position.x;
+                            place_piece((bool *) board, (bool *) screen, piece.position.x, piece.position.y, piece.piece);
                         }
                         break;
                     case EVENT_RIGHT:
-                        if (can_place_piece((bool *) board, piece_position_x + 1, current_piece_y, current_piece)) {
-                            ++piece_position_x;
-                            place_piece((bool *) board, (bool *) screen, piece_position_x, current_piece_y, current_piece);
+                        if (can_place_piece((bool *) board, piece.position.x + 1, piece.position.y, piece.piece)) {
+                            ++piece.position.x;
+                            place_piece((bool *) board, (bool *) screen, piece.position.x, piece.position.y, piece.piece);
                         }
                         break;
                     case EVENT_DOWN:
-                        if (can_place_piece((bool *) board, piece_position_x, current_piece_y + 1, current_piece)) {
-                            ++current_piece_y;
-                            place_piece((bool *) board, (bool *) screen, piece_position_x, current_piece_y, current_piece);
+                        if (can_place_piece((bool *) board, piece.position.x, piece.position.y + 1, piece.piece)) {
+                            ++piece.position.y;
+                            place_piece((bool *) board, (bool *) screen, piece.position.x, piece.position.y, piece.piece);
                         }
                         break;
                     case EVENT_UP:
@@ -159,20 +167,19 @@ int main(int argc, char** argv) {
         if (game_time == 300) {
             game_time = 0;
             memcpy(screen, board, SCREEN_X * SCREEN_Y * sizeof(bool)); 
-            if (can_place_piece((bool *) screen, piece_position_x, current_piece_y + 1, current_piece)) {
-                place_piece((bool *) board, (bool *) screen, piece_position_x, current_piece_y + 1, current_piece);
-                current_piece_y++;
+            if (can_place_piece((bool *) screen, piece.position.x, piece.position.y + 1, piece.piece)) {
+                place_piece((bool *) board, (bool *) screen, piece.position.x, piece.position.y + 1, piece.piece);
+                piece.position.y++;
             } else {
-                if (current_piece_y >= 0) {
-                    place_piece((bool *) board, (bool *) screen, piece_position_x, current_piece_y, current_piece);
+                if (piece.position.y >= 0) {
+                    place_piece((bool *) board, (bool *) screen, piece.position.x, piece.position.y, piece.piece);
                     check_board((bool *) screen, (bool *) board);
                 } else {
                     game_over((bool *) screen, (bool *) board);
                 }
-                piece_number = rand() % 4;
-                current_piece = pieces[piece_number];
-                current_piece_y = -1;
-                piece_position_x = 3;
+                piece.piece = pieces[rand() % 4];
+                piece.position.y = -1;
+                piece.position.x = 3;
             }
         }
 
